14cb2680ccc6c8ec9ab94e3c2c09627a3ebe6ad5
Cavid Ağa <57765273+cavidaga@users.noreply.github.com>
2025-09-05 01:03:51 +0200

    quick fix

--- api/analyze.js
+++ api/analyze.js
@@ -5,7 +5,6 @@ import { addExtra } from 'puppeteer-extra';
 import puppeteerCore from 'puppeteer-core';
 
 const puppeteer = addExtra(puppeteerCore);
-puppeteer.use(StealthPlugin());
 
 const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
 
